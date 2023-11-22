c7855195f41a404b380d5c51d78a588105922c95
Luiz Ianoski <124881823+ianoski7@users.noreply.github.com>
2023-11-22 07:38:44 -0300

    Add files via upload

---
+++ efeitos-sonoros/script.js
@@ -0,0 +1,44 @@
+function somChalasso1() {
+  document.querySelector("#audio_chalasso1").play();
+}
+document.querySelector(".tecla_chalasso1").onclick = somChalasso1;
+
+function somAlberti() {
+  document.querySelector("#audio_alberti").play();
+}
+document.querySelector(".tecla_alberti").onclick = somAlberti;
+
+function somEdinho() {
+  document.querySelector("#audio_edinho").play();
+}
+document.querySelector(".tecla_edinho").onclick = somEdinho;
+
+function somGui() {
+  document.querySelector("#audio_gui").play();
+}
+document.querySelector(".tecla_gui").onclick = somGui;
+
+function somJhon() {
+  document.querySelector("#audio_jhon").play();
+}
+document.querySelector(".tecla_jhon").onclick = somJhon;
+
+function somMenon() {
+  document.querySelector("#audio_menon").play();
+}
+document.querySelector(".tecla_menon").onclick = somMenon;
+
+function somPrimo() {
+  document.querySelector("#audio_primo").play();
+}
+document.querySelector(".tecla_primo").onclick = somPrimo;
+
+function somRuivo() {
+  document.querySelector("#audio_ruivo").play();
+}
+document.querySelector(".tecla_ruivo").onclick = somRuivo;
+
+function somToico() {
+  document.querySelector("#audio_toico").play();
+}
+document.querySelector(".tecla_toico").onclick = somToico;
